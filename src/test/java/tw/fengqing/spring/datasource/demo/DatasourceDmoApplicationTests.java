50b7cef6bd34a75a00a5bf4095e722baafc0e9c3
chukuolung <chukuo99@gmail.com>
2025-05-23 23:00:26 +0800

    init

---
+++ src/test/java/tw/fengqing/spring/datasource/demo/DatasourceDmoApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.fengqing.spring.datasource.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasourceDmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
